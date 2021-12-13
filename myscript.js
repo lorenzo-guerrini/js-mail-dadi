a64dc7e886aa83fab98d2fd99c5794ef0138d957
lorenzo-guerrini <lorenzoguerrini8@live.it>
2021-12-13 16:17:57 +0100

    Esercizio Mail completato con console.log

--- myscript.js
+++ myscript.js
@@ -1,3 +1,20 @@
 // Chiedi all’utente la sua email,
 // controlla che sia nella lista di chi può accedere,
 // stampa un messaggio appropriato sull’esito del controllo.
+
+const emailList = ["maurizio.costanzo@gmail.com", "mary.de-filippi@live.it", "zia.mara@libero.it"];
+const input = prompt("Qual è la tua email?");
+let found = false;
+
+for (let i = 0; i <= emailList.length; i++) {
+    if (emailList[i] == input) {
+        found = true;
+        break;
+    }
+}
+
+if (found) {
+    console.log("Puoi accedere");
+} else {
+    console.log("Non puoi accedere");
+}
